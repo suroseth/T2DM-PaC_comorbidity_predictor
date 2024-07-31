e71edf7e78da5c5cd07fc18b9988f626960a9b88
Surabhi Seth <65035385+suroseth@users.noreply.github.com>
2024-07-31 10:20:40 +0530

    Update normalization.R

--- normalization/normalization.R
+++ normalization/normalization.R
@@ -7,14 +7,13 @@ if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
 BiocManager::install("sva")
-
 library(sva)
-input_data =  NULL
+input_data =  "result_preprocessing_two_sample.csv"
 if(length(input_data) >= 1)
 {
   eset_gse15932 = read.csv("result_preprocessing_gse15932_3.csv",row.names = 1)
   eset_gse74629 = read.csv("result_preprocessing_GSE74629_3.csv",row.names = 1)
-  input_data = read.csv("input_data.csv",row.names = 1)
+  input_data = read.csv(input_data,row.names = 1)
   
   # Load your data (replace with your actual data)
   data_platform1 <- as.data.frame(eset_gse15932)
@@ -31,20 +30,21 @@ if(length(input_data) >= 1)
   data_platform2 = data_platform2[xx2,]
 
   xx3 = sapply(x1,function(i){which(rownames(input_data) == i)})
-  data_platform3 = data_platform2[xx3,]
+  data_platform3 = input_data[xx3,]
   
   identical(row.names(data_platform1),row.names(data_platform2))
-  combined_data = cbind(data_platform1,data_platform2,input_data)
+  combined_data = cbind(data_platform1,data_platform2,data_platform3)
   
   #combined_data_x = merge.data.frame(data_platform1,data_platform2)
   batch <- factor(c(rep("Platform1", ncol(data_platform1)),
                     rep("Platform2", ncol(data_platform2)),
-                    rep("Platform2", ncol(data_platform3))))
+                    rep("Platform3", ncol(data_platform3))))
   normalized_data_combat <- ComBat(combined_data, 
                                    batch = batch, 
                                    mod = NULL, 
                                    par.prior = TRUE, 
                                    prior.plots = FALSE)
+  normalized_data_combat = normalized_data_combat[,colnames(normalized_data_combat) %in% colnames(input_data)]  
 }else
   {
   eset_gse15932 = read.csv("result_preprocessing_gse15932_3.csv",row.names = 1)
@@ -71,13 +71,14 @@ if(length(input_data) >= 1)
                                    mod = NULL, 
                                    par.prior = TRUE, 
                                    prior.plots = FALSE)
-  
+  normalized_data_combat = normalized_data_combat[,colnames(normalized_data_combat) %in% colnames(input_data)]
 }
 features = read.csv("comorbidity_features_list.csv",header = FALSE)
-
 data_67_genes = normalized_data_combat[row.names(normalized_data_combat) %in% features$V1, ]
 if(length(data_67_genes[,1]) != length(features$V1))
-{print("Input data does not have all the 67 gene featurres")  }
-data_67_genes = cbind(row.names(data_67_genes),data_67_genes)
-colnames(data_67_genes)[1] = "genes"
+{
+  print("Input data does not have all the 67 gene featurres")
+  }
+data_67_genes_2 = cbind(row.names(data_67_genes),data_67_genes)
+colnames(data_67_genes_2)[1] = "genes"
 write.csv(data_67_genes,"normalized_data_combat_67features.csv",row.names = FALSE)
