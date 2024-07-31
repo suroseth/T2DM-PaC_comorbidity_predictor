9dfdc99b2a52d768069324937f3fadf83ad1f540
Surabhi Seth <65035385+suroseth@users.noreply.github.com>
2024-07-31 11:11:34 +0530

    Update pre_processing_GSE15932.R

--- pre_processing_GSE15932.R
+++ pre_processing_GSE15932.R
@@ -4,11 +4,8 @@ library(umap)
 library(WGCNA)
 library(lumi)
 library(affy)
-library(DExMA)
-library(DExMAdata)
 library(dplyr)
 library(tidyr)
-library(stringr)
 library(hgu133plus2.db)
 
 #reading affy data (.cel files) in R
@@ -29,7 +26,6 @@ Symbol <- as.list(x[ID])
 gpl570 = cbind(Symbol,ID)
 gpl570 = as.data.frame(gpl570)
 View(gpl570)
-#gpl570$Symbol[16] = "NA2"
 
 #backgroud correction/normalization/log transformation
 eset=rma(eset,normalize = TRUE,background = TRUE)
